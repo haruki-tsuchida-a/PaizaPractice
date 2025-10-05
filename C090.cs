38c7cf1c71d51c04238a12c4f5ab00bad77b4964
haruk <haruki.tf.9529@gmail.com>
2025-10-05 12:53:43 +0900

    2025/10/04

---
+++ C090.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PaizaPractice
+{
+    internal class td
+    {
+        static void ain(string[] args)
+        {
+            //0～9のダイヤル距離
+            int[] dialDistances = { 12, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            var input = Console.ReadLine();
+            int total = 0;
+
+            foreach (char c in input)
+            {
+                //入力値が数字だったら合計を足す
+                if (char.IsDigit(c))
+                {
+                    int num = int.Parse(c.ToString());
+                    total += dialDistances[num] * 2;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace PaizaPractice
 {
@@ -8,45 +6,6 @@ namespace PaizaPractice
     {
         static void Main(string[] args)
         {
-            var firstLine = Console.ReadLine().Split(' ');
-
-
-            var M = int.Parse(firstLine[0]);
-            var N = int.Parse(firstLine[1]);
-            //カード一覧
-            int[] cards = new int[M];
-            for (int i = 0; i < M; i++)
-            {
-                cards[i] = i + 1;
-            }
-
-
-            int groupCount = M / 2;
-
-
-
-            for (int i = 0; i < N; i++)
-            {
-                List<int> S = new List<int>();
-                List<int> T = new List<int>();
-
-                for (int j = 0; j < M; j++)
-                {
-                    if (j < groupCount)
-                        S.Add(cards[j]);
-                    else
-                        T.Add(cards[j]);
-                }
-
-                int[] nextCards = new int[M];
-                for (int k = 0; k < groupCount; k++)
-                {
-                    nextCards[2 * k] = T[k];
-                    nextCards[2 * k + 1] = S[k];
-                }
-                cards = nextCards; // シャッフル結果を反映
-            }
-            Console.WriteLine(string.Join(" ", cards));
 
         }
     }
